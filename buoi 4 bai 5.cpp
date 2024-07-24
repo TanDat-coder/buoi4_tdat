223a803f6291a09e2df9860776a019db304fdd2d
HUIT-sinhvien210903 <phamtandat2109@gmail.com>
2024-07-24 23:24:10 +0700

    buoi 4

---
+++ buoi 4 bai 5.cpp
@@ -0,0 +1,36 @@
+﻿//#include <stdio.h>
+//#include <string.h>
+//
+//void catChuoiHoTen(const char* hoTen, char* hoLot, char* ten) {
+//    int i, j;
+//    int length = strlen(hoTen);
+//
+//    // Tìm vị trí khoảng trắng cuối cùng
+//    for (i = length - 1; i >= 0; i--) {
+//        if (hoTen[i] == ' ') {
+//            break;
+//        }
+//    }
+//
+//    // Sao chép họ lót
+//    for (j = 0; j < i; j++) {
+//        hoLot[j] = hoTen[j];
+//    }
+//    hoLot[j] = '\0'; // Kết thúc chuỗi họ lót
+//
+//    // Sao chép tên
+//    strcpy(ten, hoTen + i + 1);
+//}
+//
+//int main() {
+//    char hoTen[] = "Nguyễn Văn Anh";
+//    char hoLot[50];
+//    char ten[50];
+//
+//    catChuoiHoTen(hoTen, hoLot, ten);
+//
+//    printf("Họ lót: %s\n", hoLot);
+//    printf("Tên: %s\n", ten);
+//
+//    return 0;
+//}
---
+++ buoi 4 bai 6.cpp
@@ -0,0 +1,109 @@
+﻿//#include <stdio.h>
+//#include <string.h>
+//
+//#define MAX 100
+//
+//typedef struct {
+//    char name[50];
+//    int age;
+//    char id[10];
+//} Student;
+//
+//void inputStudents(Student students[], int* n) {
+//    printf("Nhap so luong sinh vien: ");
+//    scanf("%d", n);
+//    for (int i = 0; i < *n; i++) {
+//        printf("Nhap thong tin sinh vien thu %d:\n", i + 1);
+//        printf("Ten: ");
+//        scanf("%s", students[i].name);
+//        printf("Tuoi: ");
+//        scanf("%d", &students[i].age);
+//        printf("Ma so sinh vien: ");
+//        scanf("%s", students[i].id);
+//    }
+//}
+//
+//void displayStudents(Student students[], int n) {
+//    printf("Danh sach sinh vien:\n");
+//    for (int i = 0; i < n; i++) {
+//        printf("Sinh vien thu %d:\n", i + 1);
+//        printf("Ten: %s\n", students[i].name);
+//        printf("Tuoi: %d\n", students[i].age);
+//        printf("Ma so sinh vien: %s\n", students[i].id);
+//    }
+//}
+//
+//void bruteForceSearch(char T[], char P[]) {
+//    int n = strlen(T);
+//    int m = strlen(P);
+//    for (int i = 0; i <= n - m; i++) {
+//        int j;
+//        for (j = 0; j < m; j++) {
+//            if (T[i + j] != P[j]) {
+//                break;
+//            }
+//        }
+//        if (j == m) {
+//            printf("Chuoi P xuat hien tai vi tri %d trong T\n", i);
+//        }
+//    }
+//}
+//
+//void searchFromKeyboard() {
+//    char T[MAX], P[MAX];
+//    printf("Nhap chuoi T: ");
+//    scanf("%s", T);
+//    printf("Nhap chuoi P: ");
+//    scanf("%s", P);
+//    bruteForceSearch(T, P);
+//}
+//
+//void searchFromFile() {
+//    char T[MAX], P[MAX];
+//    FILE* file;
+//    file = fopen("text.txt", "r");
+//    if (file == NULL) {
+//        printf("Khong the mo file\n");
+//        return;
+//    }
+//    fscanf(file, "%s", T);
+//    fclose(file);
+//    printf("Nhap chuoi P: ");
+//    scanf("%s", P);
+//    bruteForceSearch(T, P);
+//}
+//
+//int main() {
+//    Student students[MAX];
+//    int n, choice;
+//    do {
+//        printf("Menu:\n");
+//        printf("1. Nhap danh sach sinh vien\n");
+//        printf("2. Hien thi danh sach sinh vien\n");
+//        printf("3. Tim chuoi P trong T (nhap tu ban phim)\n");
+//        printf("4. Tim chuoi P trong T (doc tu file)\n");
+//        printf("5. Thoat\n");
+//        printf("Nhap lua chon cua ban: ");
+//        scanf("%d", &choice);
+//        switch (choice) {
+//        case 1:
+//            inputStudents(students, &n);
+//            break;
+//        case 2:
+//            displayStudents(students, n);
+//            break;
+//        case 3:
+//            searchFromKeyboard();
+//            break;
+//        case 4:
+//            searchFromFile();
+//            break;
+//        case 5:
+//            printf("Thoat chuong trinh\n");
+//            break;
+//        default:
+//            printf("Lua chon khong hop le\n");
+//        }
+//    } while (choice != 5);
+//    return 0;
+//}
