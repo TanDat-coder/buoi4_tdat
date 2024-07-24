51159f9630d9d06e156865dad01d3ec731689068
TanDat-coder <phamtandat2109@gmail.com>
2024-07-24 23:30:56 +0700

    Update buoi 4 bai 1.cpp

--- buoi 4 bai 1.cpp
+++ buoi 4 bai 1.cpp
@@ -1,23 +1,23 @@
-﻿//#include <stdio.h>
-//#include <ctype.h>
-//
-//int is_digit_string(const char* s) {
-//    while (*s) {
-//        if (!isdigit(*s)) {
-//            return 0; // Không phải ký số
-//        }
-//        s++;
-//    }
-//    return 1; // Toàn ký số
-//}
-//
-//int main() {
-//    const char* s = "123456";
-//    if (is_digit_string(s)) {
-//        printf("Chuoi chua toan ky so.\n");
-//    }
-//    else {
-//        printf("Chuoi khong chua toan ky so.\n");
-//    }
-//    return 0;
-//}
+﻿#include <stdio.h>
+#include <ctype.h>
+
+int is_digit_string(const char* s) {
+    while (*s) {
+        if (!isdigit(*s)) {
+            return 0; // Không phải ký số
+        }
+        s++;
+    }
+    return 1; // Toàn ký số
+}
+
+int main() {
+    const char* s = "123456";
+    if (is_digit_string(s)) {
+        printf("Chuoi chua toan ky so.\n");
+    }
+    else {
+        printf("Chuoi khong chua toan ky so.\n");
+    }
+    return 0;
+}
