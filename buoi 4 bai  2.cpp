f2487c2c047729e7df2318b5c10819adc74bc59c
TanDat-coder <phamtandat2109@gmail.com>
2024-07-24 23:30:12 +0700

    Update buoi 4 bai  2.cpp

--- buoi 4 bai  2.cpp
+++ buoi 4 bai  2.cpp
@@ -1,29 +1,29 @@
-﻿//#include <stdio.h>
-//#include <ctype.h>
-//
-//void capitalizeWords(char* str) {
-//    int inWord = 0; // Biến kiểm tra trạng thái trong từ hay không
-//
-//    while (*str) {
-//        if (isalpha(*str)) {
-//            if (!inWord) {
-//                *str = toupper(*str); // Chuyển chữ cái đầu thành in hoa
-//                inWord = 1;
-//            }
-//            else {
-//                *str = tolower(*str); // Chuyển các chữ cái còn lại thành chữ thường
-//            }
-//        }
-//        else {
-//            inWord = 0; // Kết thúc từ khi gặp ký tự không phải chữ cái
-//        }
-//        str++;
-//    }
-//}
-//
-//int main() {
-//    char str[] = "NGÀY HÔM NAY TRỜI ĐẸP";
-//    capitalizeWords(str);
-//    printf("%s\n", str);
-//    return 0;
-//}
+﻿#include <stdio.h>
+#include <ctype.h>
+
+void capitalizeWords(char* str) {
+    int inWord = 0; // Biến kiểm tra trạng thái trong từ hay không
+
+    while (*str) {
+        if (isalpha(*str)) {
+            if (!inWord) {
+                *str = toupper(*str); // Chuyển chữ cái đầu thành in hoa
+                inWord = 1;
+            }
+            else {
+                *str = tolower(*str); // Chuyển các chữ cái còn lại thành chữ thường
+            }
+        }
+        else {
+            inWord = 0; // Kết thúc từ khi gặp ký tự không phải chữ cái
+        }
+        str++;
+    }
+}
+
+int main() {
+    char str[] = "NGÀY HÔM NAY TRỜI ĐẸP";
+    capitalizeWords(str);
+    printf("%s\n", str);
+    return 0;
+}
